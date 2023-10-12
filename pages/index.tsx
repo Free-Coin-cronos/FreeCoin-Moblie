6c031a2f1f1365d6a2f09315618e8cf9f5899075
Free-Coin-cronos <146104601+Free-Coin-cronos@users.noreply.github.com>
2023-10-12 16:41:52 -0400

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -16,22 +16,23 @@ const Home: NextPage = () => {
   const { contract } = useContract(NFT_CONTRACT_ADDRESS);
   const { data: contractMetadata } = useContractMetadata(contract);
 
- const handleImageClick = () => {
+  const handleImageClick = () => {
     window.open("https://swap.crodex.app/#/swap?outputCurrency=0x4E2E7700C8f8A414b4AF8828f54330a304B6625d", "_blank", "width=700,height=600");
-};
-
+  };
 
   return (
     <div className={styles.container} style={{ display: "flex", justifyContent: "center", alignItems: "center", height: "100vh" }}>
       <a href="#" onClick={handleImageClick}>
-        <div style={{ position: "relative", width: "35%", height: "35%", left: "165px" }}> {/* Added left: "40px" */}
-          <img
-            src="/images/Thirdweb-Icon-Circle-Black-08 1.gif" // Replace with your image URL
-            alt="Image Description"
-            style={{ width: "100%", height: "100%" }}
-          />
-          <div style={{ position: "absolute", bottom: "30px", left: "0", right: "0", textAlign: "center", color: "white", fontSize: "20px" }}>
-            Click to Buy
+        <div style={{ display: "grid", placeItems: "center", width: "100%", height: "100%" }}>
+          <div style={{ display: "flex", flexDirection: "column", alignItems: "center", justifyContent: "center", marginTop: "50px" }}>
+            <img
+              src="/images/Thirdweb-Icon-Circle-Black-08 1 (3).gif" // Replace with your image URL
+              alt="Image Description"
+              style={{ width: "40%", height: "40%" }}  // Adjust the width and height here
+            />
+            <div style={{ textAlign: "center", color: "white", fontSize: "20px" }}>
+              Click to Buy
+            </div>
           </div>
         </div>
       </a>
